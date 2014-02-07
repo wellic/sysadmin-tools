c88a145ba211f6f2c39e775a6c317017319e691e
Henrik Farre <henrik@bellcom.dk>
2014-02-07 10:56:03 +0100

    Support servers without raid, try to skip cdrom

--- smartmontools-create-conf.sh
+++ smartmontools-create-conf.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-# Outputs device configuration lines to put in /etc/smartd.conf
+# Writes device configuration lines to /etc/smartd.conf
+#
+# WARNING: this script overrides the file /etc/smartd.conf
+# Requires: sg_scan (part of sg3-utils on debian) to detect adaptec raid devices
+#
 # Author Henrik Farre <hf@bellcom.dk>
 # 
 # TODO: should we use -M exec /usr/share/smartmontools/smartd-runner to run the scripts in /etc/smartmontools/run.d
+
+CONF_FILE=/etc/smartd.conf
+DEBUG=n
+
+if [[ -n $1 && $1 == '--debug' ]]; then
+  echo "Outputting to stdout, not overriding $CONF_FILE"
+  DEBUG=y
+else
+  exec > $CONF_FILE
+fi
+
+function debug {
+  if [[ $DEBUG == 'y' ]]; then
+    echo $@
+  fi
+}
 
 # -W Monitor temperature
 CONF_OPTIONS="-W 4,45,50"
@@ -16,6 +36,13 @@ HW_RAID=`lspci | grep -i raid`
 
 # Same for software
 SW_RAID=`ls /proc/mdstat 2>/dev/null`
+
+# Identify CD/DVD drive
+if [[ -f /proc/sys/dev/cdrom/info ]]; then
+  DEVICE_NAME=`grep 'drive name' /proc/sys/dev/cdrom/info | cut -f3`
+  SKIP_DEVICES="$SKIP_DEVICES $DEVICE_NAME"
+  debug "Adding " $DEVICE_NAME " to SKIP_DEVICES"
+fi
 
 if [[ -n $HW_RAID ]]; then
   HW_RAID_TYPE='unknown'
@@ -40,6 +67,24 @@ shopt -s extglob
 
 if [[ -n $SW_RAID ]]; then
   for DEVICE in /dev/disk/by-id/ata!(*part*); do
-    echo $DEVICE $CONF_OPTIONS $CONF_SCHEDULE $CONF_EMAIL
+    DEVICE_FILE=`readlink -f $DEVICE`
+    if [[ `echo $DEVICE_FILE | grep -c $SKIP_DEVICES` == 0 ]]; then
+      echo $DEVICE $CONF_OPTIONS $CONF_SCHEDULE $CONF_EMAIL
+    else
+      debug "Ignored $DEVICE_FILE"
+    fi
+  done
+fi
+
+if [[ -z $HW_RAID && -z $SW_RAID ]]; then
+  debug "No hardware or software RAID"
+
+  for DEVICE in /dev/disk/by-id/ata!(*part*); do
+    DEVICE_FILE=`readlink -f $DEVICE`
+    if [[ `echo $DEVICE_FILE | grep -c $SKIP_DEVICES` == 0 ]]; then
+      echo $DEVICE $CONF_OPTIONS $CONF_SCHEDULE $CONF_EMAIL
+    else
+      debug "Ignored $DEVICE_FILE"
+    fi
   done
 fi
